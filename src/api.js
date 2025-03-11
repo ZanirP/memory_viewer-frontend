5563a08837bf00ad75897b79e27b16461396780d
Zanir Pirani <46696445+ZanirP@users.noreply.github.com>
2025-03-11 11:53:45 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'memory-viewer-backend-production.up.railway.app'
+  baseURL: 'https://memory-viewer-backend-production.up.railway.app'
 });
 
 
